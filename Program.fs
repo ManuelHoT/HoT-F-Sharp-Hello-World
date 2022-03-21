855b07b1bbfe9cca8c89d7bb45191c1f8a140196
manuel.gonzalez <manuel.gonzalez@houseoftest.se>
2022-03-21 16:39:12 +0100

    Add project files.

---
+++ Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    let message = from "House of Test" 
+    printfn "Hello world %s" message
+    0 
